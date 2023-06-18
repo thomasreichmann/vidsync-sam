328c76145dfdfff5063b8288f6dabe9d9c9c146c
thomasreichmann <thomasalmeidar@gmail.com>
2023-06-18 20:46:59 -0300

    ,

--- concatenate/app.ts
+++ concatenate/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import fs from "fs";
 import os from "os";
 import path from "path";
 import S3Service from "./services/s3Service";
@@ -18,4 +19,45 @@ const videoService = new VideoService(OUTPUT_DIR, TEMP_DIR);
 
 export const lambdaHandler = async (_: any) => {
   let times: { [key: string]: number } = {};
+
+  // Create temp and output directories
+  if (!fs.existsSync(TEMP_DIR)) {
+    fs.mkdirSync(TEMP_DIR);
+  }
+
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+
+  let downloadPromises = ((await s3Service.list(BUCKET_FOLDER)).Contents ?? [])
+    .filter((content) => content?.Key?.endsWith(".mp4"))
+    .map((content) => s3Service.download(content.Key!, OUTPUT_DIR));
+
+  let downloadStartTime = Date.now();
+  await Promise.all(downloadPromises);
+  times["download time"] = Date.now() - downloadStartTime;
+  console.log(`Download time: ${times["download time"]}ms`);
+
+  let files = (await fs.promises.readdir(OUTPUT_DIR)).map((file) =>
+    path.join(OUTPUT_DIR, file)
+  );
+
+  let concatStartTime = Date.now();
+  await videoService.concatenateVideos(files);
+  times["concat time"] = Date.now() - concatStartTime;
+  console.log(`Concat time: ${times["concat time"]}ms`);
+  // ts
+
+  // TODO: use path returned from concat function
+  let uploadStartTime = Date.now();
+  await s3Service.upload(path.join(OUTPUT_DIR, "output.mp4"));
+  times["upload time"] = Date.now() - uploadStartTime;
+  console.log(`Upload time: ${times["upload time"]}ms`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      times,
+    }),
+  };
 };
